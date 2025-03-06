0e72b144e313ebbff64c615d27c7b85d87aec803
Harlan Williams <harlanrhwilliams@gmail.com>
2025-03-06 11:33:59 -0600

    add more test

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,5 +5,10 @@ extern "C" {
 }
 
 TEST(Factorial, ExpectedOutput) {
+    ASSERT_EQ(factorial(0), 1);
+    ASSERT_EQ(factorial(1), 1);
+    ASSERT_EQ(factorial(2), 2);
+    ASSERT_EQ(factorial(3), 6);
+    ASSERT_EQ(factorial(4), 24);
     ASSERT_EQ(factorial(5), 120);
 }
