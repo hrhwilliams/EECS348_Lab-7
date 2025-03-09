f7d1cd3eac327f9ae35e45c68810c6163f26d69c
Harlan Williams <hrw@ku.edu>
2025-03-09 11:56:41 -0500

    gjghjdfg

--- football.c
+++ football.c
@@ -2,20 +2,21 @@
 
 #include "football.h"
 
-int count_combinations(int points) {
+void count_combinations(int score) {
     int combos = 0;
+    
     //loop through possible touchdowns+2
-    for (int td2 = 0; td2 * 8 <= points; td2++) {
+    for (int td2 = 0; td2 * 8 <= score; td2++) {
         //loop through possible touchdowns+1
-        for (int td1 = 0; td1 * 7 + td2 * 8 <= points; td1++) {
+        for (int td1 = 0; td1 * 7 + td2 * 8 <= score; td1++) {
             //loop through possible touchdowns
-            for (int td = 0; td * 6 + td1 * 7 + td2 * 8 <= points; td++) {
+            for (int td = 0; td * 6 + td1 * 7 + td2 * 8 <= score; td++) {
                 //loop through possible field goals
-                for (int fg = 0; fg * 3 + td * 6 + td1 * 7 + td2 * 8 <= points; fg++) {
+                for (int fg = 0; fg * 3 + td * 6 + td1 * 7 + td2 * 8 <= score; fg++) {
                     //loop through possible safeties
-                    for (int safety = 0; safety * 2 + fg * 3 + td * 6 + td1 * 7 + td2 * 8 <= points; safety++) {
+                    for (int safety = 0; safety * 2 + fg * 3 + td * 6 + td1 * 7 + td2 * 8 <= score; safety++) {
                         int total = td2 * 8 + td1 * 7 + td * 6 + fg * 3 + safety * 2;
-                        if (total == points) {
+                        if (total == score) {
                             combos++;
                         }
                     }
@@ -23,6 +24,7 @@ int count_combinations(int points) {
             }
         }
     }
+
     return combos;
 }
 
